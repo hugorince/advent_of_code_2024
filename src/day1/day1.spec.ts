0e145f516db4411f9d3a4bfdf1057646319a854a
hugorince <hugo.rince@gmail.com>
2024-12-02 14:06:18 +0100

    day1 part1

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,5 +1,18 @@
+import { findGaps, splitColumns, turnLinesIntoArray } from "./day1";
+
 describe("day1", () => {
   it("should return true", () => {
-    expect(true).toBe(false);
+    expect(turnLinesIntoArray("df ")).toEqual(["df "]);
+  });
+
+  it("should split into 2 sorted arrays each lines", () => {
+    expect(splitColumns(["22 33", "12 13"])).toEqual({
+      firstNumbers: [12, 22],
+      secondNumbers: [13, 33],
+    });
+  });
+
+  it("should return the sum of the gaps", () => {
+    expect(findGaps([1, 2, 3], [3, 4, 5])).toEqual(6);
   });
 });
---
+++ src/day1/day1.ts
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+
+export const turnLinesIntoArray = (input: string) => {
+  return input.split("\n");
+};
+
+export const splitColumns = (array: string[]) => {
+  const firstNumbers: number[] = [];
+  const secondNumbers: number[] = [];
+
+  array.forEach((item) => {
+    const [first, second] = item.split(/\s+/).map(Number);
+
+    firstNumbers.push(first);
+    secondNumbers.push(second);
+  });
+
+  firstNumbers.sort();
+  secondNumbers.sort();
+
+  return { firstNumbers, secondNumbers };
+};
+
+const { firstNumbers, secondNumbers } = splitColumns(turnLinesIntoArray(input));
+
+export const findGaps = (array1: number[], array2: number[]): number => {
+  return array1.reduce((sum, num, index) => {
+    return sum + Math.abs(num - array2[index]);
+  }, 0);
+};
+
+console.log(findGaps(firstNumbers, secondNumbers));
