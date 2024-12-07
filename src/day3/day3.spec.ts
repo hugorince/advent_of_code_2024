27ca6ce0da72cedad5b79d4c16a813daccbc89fc
hugorince <hugo.rince@gmail.com>
2024-12-07 10:28:57 +0100

    day 3 part 2

--- src/day3/day3.spec.ts
+++ src/day3/day3.spec.ts
@@ -1,10 +1,18 @@
-import { cleanMulString, convertToArrOfNumArr, multiplyAndSum } from "./day3";
+import {
+  cleanMulString,
+  convertToArrOfNumArr,
+  convertToArrOfNumArrPart2,
+  multiplyAndSum,
+  regexPart1,
+  regexPart2,
+} from "./day3";
 
 describe("day3", () => {
   it('should clean the input and only keeps "mul(number, number)" pattern', () => {
     expect(
       cleanMulString(
-        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+        regexPart1
       )
     ).toEqual("mul(2,4)mul(5,5)mul(11,8)mul(8,5)");
   });
@@ -27,5 +35,26 @@ describe("day3", () => {
         [8, 5],
       ])
     ).toEqual(161);
+  });
+
+  it('should clean the input and only keeps "mul(number, number)" and "do()" and "dont()" pattern', () => {
+    expect(
+      cleanMulString(
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+        regexPart2
+      )
+    ).toEqual("mul(2,4)don't()mul(5,5)mul(11,8)do()mul(8,5)");
+  });
+
+  it("should turn a string to an array of number arrays and remove the ones after don't()", () => {
+    expect(
+      convertToArrOfNumArrPart2(
+        "mul(2,4)don't()mul(5,5)mul(11,8)do()mul(8,5)",
+        regexPart2
+      )
+    ).toEqual([
+      [2, 4],
+      [8, 5],
+    ]);
   });
 });
--- src/day3/day3.ts
+++ src/day3/day3.ts
@@ -3,8 +3,10 @@ const path = require("path");
 
 const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
 
-export const cleanMulString = (input: string) => {
-  const regex = /mul\(\d+,\d+\)/g;
+export const regexPart1 = /mul\(\d+,\d+\)/g;
+export const regexPart2 = /mul\(\d+,\d+\)|do\(\)|don\'t\(\)/g;
+
+export const cleanMulString = (input: string, regex: RegExp) => {
   const matches = input.match(regex);
 
   if (!matches) {
@@ -18,10 +20,28 @@ export const convertToArrOfNumArr = (str: string) => {
   const parts = str.split("mul(").filter((part) => part.length > 0);
 
   return parts.map((part) => {
-    const cleanedPart = part.slice(0, -1);
-    const numbers = cleanedPart.split(",").map(Number);
-    return numbers;
+    return part.slice(0, -1).split(",").map(Number);
   });
+};
+
+export const convertToArrOfNumArrPart2 = (input: string, regex: RegExp) => {
+  const result: number[][] = [];
+  let afterDo = true;
+  let match: RegExpExecArray | null;
+
+  while ((match = regex.exec(input)) !== null) {
+    if (match[0].startsWith("mul")) {
+      const numArr = match[0].split("mul(").join("").split(",");
+      const numbers = [parseInt(numArr[0]), parseInt(numArr[1])];
+      if (afterDo) result.push(numbers);
+    } else if (match[0] === "do()") {
+      afterDo = true;
+    } else if (match[0] === "don't()") {
+      afterDo = false;
+    }
+  }
+
+  return result;
 };
 
 export const multiplyAndSum = (arrays: number[][]) => {
@@ -34,4 +54,12 @@ export const multiplyAndSum = (arrays: number[][]) => {
   return totalSum;
 };
 
-console.log(multiplyAndSum(convertToArrOfNumArr(cleanMulString(input))));
+console.log(
+  multiplyAndSum(convertToArrOfNumArr(cleanMulString(input, regexPart1)))
+);
+
+console.log(
+  multiplyAndSum(
+    convertToArrOfNumArrPart2(cleanMulString(input, regexPart2), regexPart2)
+  )
+);
